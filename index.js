fda96084ce5d75f42138e489403c7be7f21ede2a
Love Sharma <lovesharma9179762642@gmail.com>
2025-08-15 10:41:52 +0530

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,42 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const githubData = {
+  "login": "loveCoder52",
+  "id": 194515007,
+  "node_id": "U_kgDOC5gQPw",
+  "avatar_url": "https://avatars.githubusercontent.com/u/194515007?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/loveCoder52",
+  "html_url": "https://github.com/loveCoder52",
+  "followers_url": "https://api.github.com/users/loveCoder52/followers",
+  "following_url": "https://api.github.com/users/loveCoder52/following{/other_user}",
+  "gists_url": "https://api.github.com/users/loveCoder52/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/loveCoder52/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/loveCoder52/subscriptions",
+  "organizations_url": "https://api.github.com/users/loveCoder52/orgs",
+  "repos_url": "https://api.github.com/users/loveCoder52/repos",
+  "events_url": "https://api.github.com/users/loveCoder52/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/loveCoder52/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 3,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 1,
+  "created_at": "2025-01-10T14:57:45Z",
+  "updated_at": "2025-08-15T04:28:31Z"
+};
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -17,6 +53,10 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube', (req, res) =>{
     res.send('<h1>Welcome to the YouTube page</h1>')
+})
+
+app.get('/github', (req, res) => {
+  res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
